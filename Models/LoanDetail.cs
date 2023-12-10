fa3dbf2dc2eb4ba247a51be50d938b64fd0622ba
ManSaint <sthillmanu@gmail.com>
2023-12-10 18:54:06 +0100

    Add project files.

---
+++ Models/LoanDetail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonLibraryManu.Models
+{
+    internal class LoanDetail
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NewtonLibraryManu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
